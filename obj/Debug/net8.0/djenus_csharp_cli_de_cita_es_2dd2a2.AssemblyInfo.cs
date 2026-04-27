6e8bc3527fa8ae217d294f407651b2fbe6e96e9b
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 01:01:19 -0300

    docs: changelog
    intent: docs

--- obj/Debug/net8.0/djenus_csharp_cli_de_cita_es_2dd2a2.AssemblyInfo.cs
+++ obj/Debug/net8.0/djenus_csharp_cli_de_cita_es_2dd2a2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("djenus_csharp_cli_de_cita_es_2dd2a2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5db3e7489bc52c7519b680f422d60d82537be782")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae39e7a8a6026d05093fdf909beaac33c57746fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("djenus_csharp_cli_de_cita_es_2dd2a2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("djenus_csharp_cli_de_cita_es_2dd2a2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
