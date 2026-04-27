673061cad34f2c4fabb9110e7879aa42e179e199
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 01:02:02 -0300

    feat: configuração estática
    intent: feature

--- obj/Debug/net8.0/djenus_csharp_cli_de_cita_es_2dd2a2.AssemblyInfo.cs
+++ obj/Debug/net8.0/djenus_csharp_cli_de_cita_es_2dd2a2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("djenus_csharp_cli_de_cita_es_2dd2a2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+208cb1fcd7e8fc2ab4d6d991e4d64176c70643a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6855cd3a53817156d88930ca519dc76c49a3ebc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("djenus_csharp_cli_de_cita_es_2dd2a2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("djenus_csharp_cli_de_cita_es_2dd2a2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
